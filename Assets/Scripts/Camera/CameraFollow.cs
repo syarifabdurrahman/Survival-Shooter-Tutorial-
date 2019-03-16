7c87d1d7bb78214488e42e0b5e5285df01a4fbca
Syarif abdurrahman(noob snake) <syarifabdr22@gmail.com>
2019-03-16 08:44:42 +0700

    survival shooter

---
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+    public float Smoothing = 5f;
+
+    Vector3 offset;
+
+    private void Start()
+    {
+        offset = transform.position - target.position;
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 TargetCampos = target.position + offset;
+        transform.position = Vector3.Lerp(transform.position, TargetCampos,Smoothing*Time.deltaTime);
+    }
+}
